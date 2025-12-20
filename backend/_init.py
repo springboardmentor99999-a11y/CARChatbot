916e69878acc77f8f5cd4712460dab3bd53a859b
Khushi S U <khushisu192@gmail.com>
2025-12-20 16:47:02 +0530

    Initial commit from my local folder

---
+++ backend/_init.py
@@ -0,0 +1,18 @@
+from backend.db import save_contract
+from fastapi import FastAPI, UploadFile, File
+from backend.contract_analyser import analyze_contract
+from backend.pdf_reader import extract_text_from_pdf
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API is running"}
+
+@app.post("/analyze")
+async def analyze_contract_api(file: UploadFile):
+    pdf_bytes = await file.read()
+    text = extract_text_from_pdf(pdf_bytes)
+    save_contract(file.filename,text)
+    result = analyze_contract(text)
+    return {"analysis": result}
--- backend/pdf_reader.py
+++ backend/pdf_reader.py
@@ -1,9 +1,18 @@
-from PyPDF2 import PdfReader
-from io import BytesIO
+import pdfplumber
+import os
 
-def extract_text_from_pdf(pdf_bytes):
-    reader = PdfReader(BytesIO(pdf_bytes))
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SAMPLES_DIR = os.path.join(BASE_DIR, "samples")
+
+def extract_text_from_pdf(filename: str) -> str:
+    pdf_path = os.path.join(SAMPLES_DIR, filename)
+
+    text = []
+    with pdfplumber.open(pdf_path) as pdf:
+        for i, page in enumerate(pdf.pages, start=1):
+            page_text = page.extract_text()
+            if page_text:
+                text.append(f"\n--- Page {i} ---\n")
+                text.append(page_text)
+
+    return "\n".join(text)
