78f5b5476946f7d807575c7c2a93cf1b5a4de5fc
samira-jannat-143231 <samira279803@gmail.com>
2025-12-27 20:37:49 +0530

    Milestone-2 completed, updated on 27 december

--- backend/contract_analyzer.py
+++ backend/contract_analyzer.py
@@ -1,3 +1,20 @@
+from dotenv import load_dotenv
+import os
+from langchain_ollama import ChatOllama
+
+#Load .env
+current_dir = os.path.dirname(__file__)
+env_path = os.path.join(current_dir, ".env")
+load_dotenv(env_path)
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+print("API KEY LOADED:", OPENAI_API_KEY is not None)
+
+llm = ChatOllama(
+    temperature=0,
+    model="qwen3:8b"
+)
+
 
 def analyze_contract(text: str):
     return {
