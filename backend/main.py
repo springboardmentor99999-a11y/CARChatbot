9eb2632e0dd4c837d15b053c99af1cf5eba8e783
Kishorsivakumar <kishorsiva7777@gmail.com>
2025-12-27 17:27:38 +0530

    Milestone 2: Backend API.

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from fastapi import FastAPI, UploadFile
 from .contract_analyser import analyze_contract
 from .pdf_reader import extract_text_from_pdf
@@ -16,3 +17,22 @@ async def analyze_contract_api(file: UploadFile):
     text = extract_text_from_pdf(pdf_bytes)
     result = analyze_contract(text)
     return {"analysis": result}
+=======
+from pdf_reader import extract_text_from_pdf
+from db import save_contract
+from contract_analyser import analyze_contract
+
+def ingest_pdf(pdf_filename):
+    print("📄 Extracting PDF text...")
+    text = extract_text_from_pdf(f"samples/{pdf_filename}")
+
+    print("💾 Saving to database...")
+    save_contract(pdf_filename, text)
+
+    result = analyze_contract(text)
+    print("📊 ANALYZED CONTRACT DATA")
+    print(result)
+
+if __name__ == "__main__":
+    ingest_pdf("sample_contract.pdf")
+>>>>>>> 7170000 (Milestone 2: Backend API.)
